83c8bfe6c386198e87d09452f61c7ddc74f568e4
Wojciech Warchol <Wojciech.Warchol@motorolasolutions.com>
2020-05-16 09:08:22 +0200

    refactored StockCalculators

--- src/com/wojto/calculations/GpwStockCalculator.java
+++ src/com/wojto/calculations/GpwStockCalculator.java
@@ -14,28 +14,17 @@ public class GpwStockCalculator implements StockCalculator {
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String FORMAT = "%s%-15s%s%-16s%s%10s%n";
 
-    private Stock stock;
-    private StockPerformance stockPerformance;
-
-    public GpwStockCalculator(Stock stock, StockPerformance stockPerformance) {
-        this.stock = stock;
-        this.stockPerformance = stockPerformance;
-    }
-
-    public GpwStockCalculator(Stock stock) {
-        this.stock = stock;
-        this.stockPerformance = new StockPerformance(stock.getStockName());
-    }
+    public GpwStockCalculator() {}
 
     @Override
-    public void calculate() {
+    public StockPerformance calculate(Stock stock) {
         // TODO Include provisions
         int quantityOwned = 0;
         BigDecimal presentValue = BigDecimal.ZERO;
+        StockPerformance stockPerformance = new StockPerformance();
 
         for (Transaction transaction : stock.getTransactions()) {
             if(transaction.getTransactionType() == TransactionType.BUY) {
-//                stockPerformance.addPucheseToOpenPosition(transaction.getTotalValue());
                 quantityOwned += transaction.getAmount();
                 presentValue = presentValue.add(transaction.getTotalValue());
             } else if(transaction.getTransactionType() == TransactionType.SELL) {
@@ -44,7 +33,7 @@ public class GpwStockCalculator implements StockCalculator {
 
                 } else if (quantityOwned == 0) {
                     presentValue = presentValue.subtract(transaction.getTotalValue());
-                    stockPerformance.setInvestmenResault(presentValue.negate());
+                    stockPerformance.updateInvestmentResault(presentValue.negate());
                     presentValue = BigDecimal.ZERO;
                 } else {
                     presentValue = presentValue.subtract(transaction.getTotalValue());
@@ -54,6 +43,7 @@ public class GpwStockCalculator implements StockCalculator {
 
         stockPerformance.setOpenPositionValue(presentValue);
         stockPerformance.setOpenPositionAmount(quantityOwned);
+        return stockPerformance;
     }
 
     //TODO Extract to seperate class, seperate Lacked, Opened and Closed Transactions.
@@ -67,18 +57,6 @@ public class GpwStockCalculator implements StockCalculator {
                 "Performance: ", colorInvestmentResault(stockPerformance.getInvestmenResault()));
     }
 
-    public LocalDateTime getLastTransactionDate() {
-        LocalDateTime lastDate = LocalDateTime.MIN;
-        LocalDateTime dateToCheck;
-        LocalDateTime lastTransactionDate = null;
-        for (Transaction transaction : stock.getTransactions()) {
-            dateToCheck = transaction.getTransactionDate();
-            lastTransactionDate = dateToCheck.compareTo(lastDate) > 0 ? dateToCheck : lastDate;
-            lastDate = dateToCheck;
-        }
-        return lastTransactionDate;
-    }
-
     public String colorInvestmentResault(BigDecimal investmentResault) {
         String coloredResault = String.format("%10s", investmentResault);
         if (investmentResault.compareTo(BigDecimal.ZERO) > 0) {
@@ -87,22 +65,6 @@ public class GpwStockCalculator implements StockCalculator {
             coloredResault = ANSI_RED + coloredResault + ANSI_RESET;
         }
         return coloredResault;
-    }
-
-    public Stock getStock() {
-        return stock;
-    }
-
-    public void setStock(Stock stock) {
-        this.stock = stock;
-    }
-
-    public StockPerformance getStockPerformance() {
-        return stockPerformance;
-    }
-
-    public void setStockPerformance(StockPerformance stockPerformance) {
-        this.stockPerformance = stockPerformance;
     }
 
 
