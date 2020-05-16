fbb0371bd9b02412a4a3c8d13e83707424612346
Wojciech Warchol <Wojciech.Warchol@motorolasolutions.com>
2020-05-16 08:16:19 +0200

    started stock sorting method

--- src/com/wojto/calculations/StockPortoflio.java
+++ src/com/wojto/calculations/StockPortoflio.java
@@ -1,5 +1,6 @@
 package com.wojto.calculations;
 
+import com.wojto.model.StateOfPossesion;
 import com.wojto.model.Stock;
 import com.wojto.model.Transaction;
 
@@ -42,7 +43,26 @@ public class StockPortoflio {
     }
 
     private void sortStocksByStatusAndPerformance() {
+        // TODO use streams to do this
+        List<GpwStockCalculator> openPositions = new ArrayList<>();
+        List<GpwStockCalculator> closedPostions = new ArrayList<>();
+        List<GpwStockCalculator> lackingPositons = new ArrayList<>();
+        getAllStocksOfThisState(openPositions, StateOfPossesion.OPEN);
+        getAllStocksOfThisState(closedPostions, StateOfPossesion.CLOSED);
+        getAllStocksOfThisState(lackingPositons, StateOfPossesion.LACKS_PURCHESE);
+        List<StockCalculator> sortedList = new ArrayList<>();
+        sortedList.add(openPositions);
+        sortedList.add(closedPostions);
+        sortedList.add(lackingPositons);
+    }
 
+    private void getAllStocksOfThisState(List<GpwStockCalculator> list, StateOfPossesion state) {
+        for (StockCalculator stockCalculator : stockCalculators) {
+            GpwStockCalculator gpwStockCalculator = (GpwStockCalculator) stockCalculator;
+            if ( gpwStockCalculator.getStock().getStateOfPossesion() == state) {
+                list.add(gpwStockCalculator);
+            }
+        }
     }
 
     public StockCalculator getStockFromSymbol(String string) {
