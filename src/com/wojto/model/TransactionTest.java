a3672cbe60b154188e33c8dc9baa19a8559dd1b4
Wojciech Warchol <Wojciech.Warchol@motorolasolutions.com>
2020-07-22 11:02:21 +0200

    refactored test

--- src/com/wojto/model/TransactionTest.java
+++ src/com/wojto/model/TransactionTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class TransactionTest {
 
     static private LocalDateTime TODAY_NOW = LocalDateTime.of(2020, Month.JANUARY, 1, 12, 00);
-    static private LocalDateTime LATER_TODAY = LocalDateTime.now().plusMinutes(1);
+    static private LocalDateTime LATER_TODAY = TODAY_NOW.plusMinutes(1);
     static private LocalDateTime NEXT_DAY = LocalDateTime.of(2020, Month.JANUARY, 2, 13, 00);
 
     static private Transaction BUY_NOW = new Transaction(TODAY_NOW, "S1", "GPW", TransactionType.BUY, 1, BigDecimal.ONE, BigDecimal.ONE);
@@ -28,11 +28,23 @@ class TransactionTest {
     }
 
     @Test
-    void isSameDayAndTypeAs() {
+    void isSameDayAndTypeAsSameDayTransaction() {
         assertEquals(true, BUY_NOW.isSameDayAndTypeAs(BUY_NOW));
         assertEquals(true, BUY_NOW.isSameDayAndTypeAs(BUY_LATER));
+    }
+
+    @Test
+    void isSameDayAndTypeOnDifferentDayTransaction() {
         assertEquals(false, BUY_NOW.isSameDayAndTypeAs(BUY_NEXT_DAY));
+    }
+
+    @Test
+    void isSameDayAndTypeOnDifferentTypeTransaction() {
         assertEquals(false, BUY_NOW.isSameDayAndTypeAs(SELL_NOW));
+    }
+
+    @Test
+    void isSameDayAndTypeOnDifferentDayAndTypeTransaction() {
         assertEquals(false, BUY_NOW.isSameDayAndTypeAs(SELL_NEXT_DAY));
     }
 }
